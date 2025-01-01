3edc639b108c616cdf380453ae713751a550b044
Benjamin R Cope <brc1379@gmail.com>
2025-01-01 14:52:02 -0500

    Started working on physics

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -6,5 +6,3 @@ func _ready():
 func _process(_delta):
 	if $Sprite2D.position.x >= 1000:
 		$Sprite2D.position.x = 0
-		
-	#if Input.is_action_pressed("left"):
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -1,10 +1,15 @@
-extends Node2D
+extends CharacterBody2D
 
 const speed = 300
 
 func _process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	position += direction * speed * delta
+	velocity = direction * speed * delta
+	move_and_slide()
+	#position += direction * speed * delta
 	
 	if Input.is_action_just_pressed("primary action"):
 		print("pew pew")
+		
+	if Input.is_action_just_pressed("secondary action"):
+		print("shoot grenade")
