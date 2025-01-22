06fe4fba6dfc743bf690f9285c2f8b688d43df4c
Benjamin R Cope <brc1379@gmail.com>
2025-01-21 19:20:38 -0500

    Added grenade explosion

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -1,3 +1,10 @@
 extends RigidBody2D
 
 var speed = 750
+
+#func _init(delta: float) -> void:
+	#$Explosion.visible = false
+
+func explode():
+	$Explosion.visible = true
+	$AnimationPlayer.play("Explosion")
