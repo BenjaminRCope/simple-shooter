d41025573c249bc5eada4efd31c25a5ccad2acbb
Benjamin R Cope <brc1379@gmail.com>
2025-01-30 18:51:26 -0500

    Lights, basic scene transitions

---
+++ scenes/levels/inside.gd
@@ -0,0 +1,6 @@
+extends LevelParent
+
+func _on_scene_transition_body_entered(_body: Node2D) -> void:
+	var tween = create_tween()
+	tween.tween_property($Player, "speed", 0, 0.5)
+	get_tree().change_scene_to_file("res://scenes/levels/outside.tscn")
