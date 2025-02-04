554a9634963a354a934bd9b73ab67895337c6125
Benjamin R Cope <brc1379@gmail.com>
2025-02-03 21:32:33 -0500

    Working on UI, grenades

--- scenes/projectiles/grenade.gd
+++ scenes/projectiles/grenade.gd
@@ -5,5 +5,7 @@ var speed = 750
 func explode():
 	$Sprite2D.visible = false
 	$PointLight2D.visible = false
+	$".".sleeping = true
 	$AnimationPlayer.play("Explosion")
+	$CollisionShape2D.disabled = true
 	$Explosion.visible = true
