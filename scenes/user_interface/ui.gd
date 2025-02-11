5808576c70bfd34bb112e505686b08cdcd80ac51
Benjamin R Cope <brc1379@gmail.com>
2025-02-11 18:27:55 -0500

    better handling with ui changes

--- scenes/user_interface/ui.gd
+++ scenes/user_interface/ui.gd
@@ -6,10 +6,10 @@ var red: Color = Color("FF0000")
 
 @onready var laser_label: Label = $LaserCounter/VBoxContainer/Label
 @onready var grenade_label: Label = $GrenadeCounter/VBoxContainer/Label
+@onready var health_bar: TextureProgressBar = $MarginContainer/TextureProgressBar
 
 func _ready():
-	update_laser_text()
-	update_grenade_text()
+	Globals.connect("stat_change", update_stat_text)
 
 func update_laser_text():
 	laser_label.text = str(Globals.laser_amount)
@@ -18,6 +18,14 @@ func update_laser_text():
 func update_grenade_text():
 	grenade_label.text = str(Globals.grenade_amount)
 	update_color(Globals.grenade_amount, $GrenadeCounter/VBoxContainer)
+	
+func update_health_text():
+	health_bar.value = Globals.health
+	
+func update_stat_text():
+	update_health_text()
+	update_laser_text()
+	update_grenade_text()
 
 func update_color(amount: int, container: VBoxContainer) -> void:
 	if amount <= 0:
